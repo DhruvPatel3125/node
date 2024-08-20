afed7cac6b71fbc48f663fe80a820d10fffc6818
DhruvPatel3125 <159695953+DhruvPatel3125@users.noreply.github.com>
2024-08-20 21:40:59 +0530

    Add files via upload

---
+++ math.js
@@ -0,0 +1,10 @@
+const sum = (a,b)=>a+b;
+const mul = (a,b)=>a*b;
+const pi = 3.14;
+
+let obj ={
+    sum:sum,
+    mul:mul,
+    pi:pi
+};
+module.exports = obj;
---
+++ script.js
@@ -0,0 +1,20 @@
+// let n = 5;
+
+// for(i=0;i<n;i++){
+//     console.log("hello ",i);
+// }
+
+// console.log("bye!")
+
+// console.log(process.argv);
+
+// let args = process.argv;
+
+// for(let i=2;i<args.length;i++){
+//     console.log("hello to",args[i])
+// }
+
+
+const math = require("./math.js");
+
+console.log((math)); 
